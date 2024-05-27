d6f705b000684902868d89ff0bba9764cd5c0f14
OnsKoukiPro <ons.koukipro@yahoo.com>
2024-05-27 11:35:07 +0100

    add loginHandler

--- src/app/(web)/auth/page.tsx
+++ src/app/(web)/auth/page.tsx
@@ -17,6 +17,25 @@ const Auth = () => {
         const {name, value} = event.target;
         setFormData({ ...formData, [name]: value});
     };
+
+    const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) router.push('/');
+  }, [router, session]);
+
+  const loginHandler = async () => {
+    try {
+      await signIn();
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+      toast.error("Something wen't wrong");
+    }
+  };
+
+
     const handleSubmit = async (event: FormEvent<HTMLFormElement>)  =>{
         event.preventDefault();
 
@@ -33,17 +52,23 @@ const Auth = () => {
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
             <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
                 <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
-                    Ceate an account
+                    Create an account
                 </h1>
                 <p>OR</p>
                 <span className="inline-flex items-center">
-                    <AiFillGithub className='mr-3 text-4xl cursor-pointer text-black dark:text-white' />
-                    <FcGoogle className="ml-3 text-4xl cursor-pointer"/>
+                    <AiFillGithub
+                        onClick={loginHandler}
+                        className='mr-3 text-4xl cursor-pointer text-black dark:text-white'
+                        />{' '}
+                    <FcGoogle
+                        onClick={loginHandler}
+                        className="ml-3 text-4xl cursor-pointer"
+                    />
                 </span>
             </div>
             <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
-                
-                <input 
+
+                <input
                 type='text'
                 name='name'
                 placeholder='INSAT'
@@ -52,7 +77,7 @@ const Auth = () => {
                 value={formData.name}
                 onChange={handleInputChange}
                 />
-                <input 
+                <input
                 type='email'
                 name='email'
                 placeholder='name.lastname@insat.ucar.tn'
@@ -61,7 +86,7 @@ const Auth = () => {
                 value={formData.email}
                 onChange={handleInputChange}
                  />
-                 <input 
+                 <input
                 type='password'
                 name='password'
                 placeholder='password'
