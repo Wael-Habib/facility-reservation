67d134853f97b34b7b510105f4cc1586032bf282
Wael-Habib <wael.habib.tn@gmail.com>
2024-06-03 17:39:24 +0100

    Final Final Final

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -9,6 +9,23 @@ import {
   updateReview,
 } from '@/libs/apis';
 
+export async function GET(req: Request, res: Response) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new NextResponse('Authentication Required', { status: 500 });
+  }
+
+  const userId = session.user.id;
+
+  try {
+    const data = await getUserData(userId);
+    return NextResponse.json(data, { status: 200, statusText: 'Successful' });
+  } catch (error) {
+    return new NextResponse('Unable to fetch', { status: 400 });
+  }
+}
+
 export async function POST(req: Request, res: Response) {
   const session = await getServerSession(authOptions);
 
