c841397bfa1564537086ffa7781f154637279450
Wael-Habib <wael.habib.tn@gmail.com>
2024-06-03 13:33:44 +0100

    Final last

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -3,62 +3,6 @@ import { authOptions } from '@/libs/auth';
 import { getServerSession } from 'next-auth';
 import { getFacility, createBooking, updateFacility } from '@/libs/apis';
 
-
-export default async function GET(req: Request, res: Response) {
-  const {
-    checkinDate, 
-    checkoutDate, 
-    numberOfDays, 
-    participants,
-    Slug,
-  }  = await req.json();
-  console.log("checkinDate: ",checkinDate);
-  try {
-    if (
-      !checkinDate ||
-      !checkoutDate ||
-      !participants ||
-      !numberOfDays ||
-      !Slug
-    ) {
-      return new NextResponse('Please provide all required fields', {
-        status: 400,
-      });
-    }
-    const origin = req.headers.get('origin');
-    console.log("test");
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      return new NextResponse('Authentication required', { status: 400 });
-    }
-    console.log("Session: ",session);
-    const userId = session.user.id;
-    const facility = await getFacility(Slug);
-    const facilityId = facility._id;
-    await createBooking({
-      userId,
-      facilityId,
-      checkinDate,
-      checkoutDate,
-      participants,
-      numberOfDays,
-    });
-
-    // Update facility
-    await updateFacility(facilityId);
-
-    return new NextResponse('Booking successful', {
-      status: 200,
-      statusText: 'Booking Successful',
-    });
-  } catch (error) {
-    console.error('Error occurred during booking:', error);
-    return new NextResponse('Booking failed', {
-      status: 500,
-      statusText: 'Booking Failed',
-    });
-  }
-}
 export async function POST(req: Request, res: Response) {
   const session = await getServerSession(authOptions);
   console.log("session: ",session);
